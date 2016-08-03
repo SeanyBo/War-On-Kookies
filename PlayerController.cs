3504650e508d64629bee9bad9b22354e64543485
msibal6 <mitchell.sibal@gmail.com>
2016-08-02 18:01:31 -0700

    Add files via upload
    Controls the Girl Animation

---
+++ PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+	
+	private Animator animator;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		animator = this.GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+		var vertical = Input.GetAxis("Vertical");
+		var horizontal = Input.GetAxis("Horizontal");
+
+		if (vertical > 0)
+		{
+			animator.SetInteger("Direction", 2);
+		}
+		else if (vertical < 0)
+		{
+			animator.SetInteger("Direction", 0);
+		}
+		else if (horizontal > 0)
+		{
+			animator.SetInteger("Direction", 3);
+		}
+		else if (horizontal < 0)
+		{
+			animator.SetInteger("Direction", 1);
+		}
+	}
+
+
+}
